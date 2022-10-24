993b88781ca1bc774c971fb30be595d77b97fd13
Ahmet Fatih Çopur <afatih98@hotmail.com>
2022-10-25 00:55:01 +0300

    calculateCircle Finished

--- Usage of Command Line/calculateCircle.js
+++ Usage of Command Line/calculateCircle.js
@@ -1,1 +1,10 @@
-console.log("merhabalar");
+
+const arguments = process.argv.slice(2);
+
+function calculate(radius){
+
+    const circleArea = radius*radius*3.14;
+    console.log(circleArea);
+}
+
+calculate(arguments[0] * 1);
